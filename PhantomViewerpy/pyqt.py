9d27c8ec5edf6f6fd7655ed9a352585f97d54228
sapje1 <evansj31@cardiff.ac.uk>
2023-08-09 09:17:24 +0100

    mods to get qt working on wl026

--- PhantomViewerpy/pyqt.py
+++ PhantomViewerpy/pyqt.py
@@ -4,8 +4,7 @@
 
 
 pyqtVersion=5    #flag to indicate which pyqt version is in use
-from PyQt5.Qt import PYQT_VERSION_STR
-from PyQt5.QtCore import  Qt, QPoint
+from PyQt5.QtCore import  Qt, QPoint, PYQT_VERSION_STR
 from PyQt5.QtGui import  QFont, QColor, QPainter, QPixmap, QTextOption, QScreen
 from PyQt5.QtWidgets import QApplication, QMainWindow,  QWidget, QProgressDialog, QInputDialog, QColorDialog, \
   QLineEdit, QFileDialog, QAction, QTextEdit, QToolTip, QStatusBar, QMenuBar, QMenu, QStyleFactory
